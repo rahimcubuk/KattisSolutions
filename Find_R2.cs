2a31c2cd11871e335d9b45a87548eb1280c0d2ad
rahim çubuk <rahimcubuk@gmail.com>
2020-10-23 19:22:40 +0300

    KattisSolutions

---
+++ Find_R2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Find_R2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[2];
+            string[] words = Console.ReadLine().Split(' ');
+
+            int i = 0;
+            foreach (var word in words)
+            {
+                numbers[i] = int.Parse($"{word}");
+                i++;
+            }
+            findR2(numbers);
+            Console.ReadKey();
+        }
+
+        static void findR2(int[] numbers)
+        {
+            Console.WriteLine(2 * numbers[1] - numbers[0]);
+        }
+    }
+}
---
+++ Hello_World.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum_E = 0;
+            int sum_G = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                sum_G = sum_G + Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                sum_E = sum_E + Convert.ToInt32(Console.ReadLine());
+            }
+            if (sum_E > sum_G) Console.WriteLine("Emma");
+            else if (sum_E < sum_G) Console.WriteLine("Gunnar");
+            else Console.WriteLine("Tie");
+        }
+    }
+}
